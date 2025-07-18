c79346f0df7b3d8b7640b494f18865e9e0a80cc9
Caio Vellani <caiovellani@outlook.com>
2025-07-18 19:56:54 -0300

    feat: add toas notifications to category search states

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,5 +1,8 @@
+import { createStandaloneToast } from '@chakra-ui/toast'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import categoryServices from 'services/categorias'
+
+const { toast } = createStandaloneToast()
 
 const initialState = []
 
@@ -14,12 +17,33 @@ const categorySlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(searchCategory.fulfilled, (state, { payload }) => {
+        toast({
+          title: 'Sucesso!',
+          description: 'Categorias carregadas com sucesso!',
+          duration: 2000,
+          isClosable: true,
+          status: 'success',
+        })
         return payload
       })
       .addCase(searchCategory.pending, (state, { payload }) => {
+        toast({
+          title: 'Carregando!',
+          description: 'Carregando categorias!',
+          duration: 2000,
+          isClosable: true,
+          status: 'loading',
+        })
         console.log('carregando categorias.')
       })
       .addCase(searchCategory.rejected, (state, { payload }) => {
+        toast({
+          title: 'Error!',
+          description: 'Erro na busca de categorias!',
+          duration: 2000,
+          isClosable: true,
+          status: 'error',
+        })
         console.log('busca de categoria rejeitada.')
       })
   },
